f78fa0641783a3d784bd00a573d52abe01dd0096
Sergei Belash <BelashSN@outlook.com>
2025-04-29 16:47:18 +0300

    Init commit

---
+++ upload.php
@@ -0,0 +1,42 @@
+<?php
+    ini_set('display_errors','Off');
+    error_reporting(E_ALL);
+    
+    // -- Проверим, передан ли файл, или вернем ошибку
+    if ($_FILES['content']['size'] === 0) {
+        sendresult('Файл для сохранения не выбран');
+    }
+
+    // -- Проверим, передано ли имя сохранения, или вернем ошибку
+    if (empty($_POST['file_name'])) {
+        sendresult('Имя для сохранения файла не выбрано');
+    }
+    
+    // -- Установим папку - приемник файлов
+    $uploadDir = __DIR__ . '/files/';
+
+    // -- Создадим папку, если она отсутсвует
+    if(!is_dir($uploadDir)) {
+        mkdir($uploadDir, 0777, true);
+    }
+
+    // -- Получим расширение файла - источника
+    $sourceEx = pathinfo($_FILES['content']['name'])['extension']; 
+    // -- Получим расширение файла - приемника
+    $receptEx = pathinfo($_POST['file_name'])['extension']; 
+    // -- Получим итоговое имя файла
+    $uploadFile = $_POST['file_name'] . ($sourceEx === $receptEx ? '' : ('.' . $sourceEx));
+    
+    if (file_exists(filename: $uploadDir . $uploadFile)) {
+        sendresult('Файл "' . $uploadFile . '" уже существует...');
+    } elseif (move_uploaded_file($_FILES['content']['tmp_name'],$uploadDir . $uploadFile)) {
+        sendresult('Файл успешно сохранен:|||' . $uploadDir . $uploadFile);
+    } else {
+        sendresult('Не удалось сохранить файл: ' . $uploadFile);
+    }
+    
+    /* ----------- Перейдем на исходную страницу с возвратом ответа Get --------- */
+    function sendresult(string $result): void {
+        header("Location: " . $_SERVER["HTTP_REFERER"] . '?res=' . $result);
+        exit;
+    }
